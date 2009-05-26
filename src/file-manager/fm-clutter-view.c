a5bf354d68eadbea8554a9e6b578004aa4ac6f52
John Stowers <john.stowers@gmail.com>
2009-05-26 19:26:12 +1200

    Add the clutter view to the stage

--- src/file-manager/fm-clutter-view.c
+++ src/file-manager/fm-clutter-view.c
@@ -36,18 +36,14 @@
 
 #include <clutter/clutter.h>
 #include <clutter-gtk.h>
+#include "clutter-cover-flow.h"
 
-#define NHANDS  2
-#define WINWIDTH   400
-#define WINHEIGHT  400
-#define RADIUS     150
+#define WINWIDTH   1200
+#define WINHEIGHT  500
 
 struct FMClutterViewDetails {
 	int number_of_files;
-	ClutterActor *hand[NHANDS];
-	ClutterActor *bgtex;
-	ClutterGroup *group;
-	GdkPixbuf *bgpixb;
+	ClutterCoverFlow *cf;
 	GtkWidget *clutter;
 };
 
@@ -67,7 +63,7 @@ G_DEFINE_TYPE_WITH_CODE (FMClutterView, fm_clutter_view, FM_TYPE_DIRECTORY_VIEW,
 static void
 fm_clutter_view_add_file (FMDirectoryView *view, NautilusFile *file, NautilusDirectory *directory)
 {
-	//FM_CLUTTER_VIEW (view)->details->number_of_files++;
+	FM_CLUTTER_VIEW (view)->details->number_of_files++;
 }
 
 
@@ -338,40 +334,10 @@ fm_clutter_view_iface_init (NautilusViewIface *iface)
 
 
 static void
-frame_cb (ClutterTimeline *timeline, gint frame_num, gpointer data)
-{
-	FMClutterViewDetails *oh = (FMClutterViewDetails *)data;
-	gint            i;
-
-	/* Rotate everything clockwise about stage center*/
-	clutter_actor_set_rotation (CLUTTER_ACTOR (oh->group),
-		              CLUTTER_Z_AXIS,
-		              frame_num,
-		              WINWIDTH / 2, WINHEIGHT / 2, 0);
-
-	for (i = 0; i < NHANDS; i++)
-	{
-	/* rotate each hand around there centers */
-	clutter_actor_set_rotation (oh->hand[i],
-		                  CLUTTER_Z_AXIS,
-		                  - 6.0 * frame_num,
-		                  clutter_actor_get_width (oh->hand[i]) / 2,
-		                  clutter_actor_get_height (oh->hand[i]) / 2,
-		                  0);
-	//if (fade == TRUE)
-	//  clutter_actor_set_opacity (oh->hand[i], (255 - (frame_num % 255)));
-	}
-
-}
-
-static void
 fm_clutter_view_init (FMClutterView *empty_view)
 {
-	ClutterTimeline *timeline;
 	ClutterActor    *stage;
-	ClutterColor     stage_color = { 0x61, 0x64, 0x8c, 0xff };
-	GdkPixbuf       *pixbuf;
-	gint             i;
+	ClutterColor     stage_color = { 0x00, 0x00, 0x00, 0xff }; /* black */
 
 	if (gtk_clutter_init (NULL, NULL) != CLUTTER_INIT_SUCCESS) {
 		g_error ("Unable to initialize GtkClutter");
@@ -379,10 +345,6 @@ fm_clutter_view_init (FMClutterView *empty_view)
 	}
 	
 	empty_view->details = g_new0 (FMClutterViewDetails, 1);
-	pixbuf = gdk_pixbuf_new_from_file ("/home/john/Desktop/clutter-gtk-0.9.0/examples/redhand.png", NULL);
-
-	if (!pixbuf)
-		g_error("pixbuf load failed");
 
 	empty_view->details->clutter = gtk_clutter_embed_new ();
 	gtk_widget_set_size_request (empty_view->details->clutter, WINWIDTH, WINHEIGHT);
@@ -390,61 +352,25 @@ fm_clutter_view_init (FMClutterView *empty_view)
 	gtk_container_add (GTK_CONTAINER (empty_view), GTK_WIDGET (empty_view->details->clutter));
 	stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (empty_view->details->clutter));
 
-	clutter_stage_set_color (CLUTTER_STAGE (stage),
-			   &stage_color);
+	clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
 
-  	/* create a new group to hold multiple actors in a group */
-  	empty_view->details->group = CLUTTER_GROUP (clutter_group_new ());
+  	/* create the cover flow widget */
+  	empty_view->details->cf = clutter_cover_flow_new ( CLUTTER_ACTOR (stage) );
   
-	for (i = 0; i < NHANDS; i++)
-	{
-		gint x, y, w, h;
-
-		/* Create a texture from pixbuf, then clone in to same resources */
-		if (i == 0)
-			empty_view->details->hand[i] = gtk_clutter_texture_new_from_pixbuf (pixbuf);
-		else
-			empty_view->details->hand[i] = clutter_clone_new (empty_view->details->hand[0]);
-		/* Place around a circle */
-		w = clutter_actor_get_width (empty_view->details->hand[0]);
-		h = clutter_actor_get_height (empty_view->details->hand[0]);
-
-		x = WINWIDTH/2  + RADIUS * cos (i * M_PI / (NHANDS/2)) - w/2;
-		y = WINHEIGHT/2 + RADIUS * sin (i * M_PI / (NHANDS/2)) - h/2;
-
-		clutter_actor_set_position (empty_view->details->hand[i], x, y);
-
-		/* Add to our group group */
-		clutter_group_add (empty_view->details->group, empty_view->details->hand[i]);
-	}
-
-	/* Add the group to the stage */
+	/* Add the widget to the stage */
 	clutter_container_add_actor (CLUTTER_CONTAINER (stage),
-		               CLUTTER_ACTOR (empty_view->details->group));
-
-	//g_signal_connect (stage, "button-press-event",
-	//	    G_CALLBACK (input_cb), 
-	//	    oh);
-	//g_signal_connect (stage, "key-release-event",
-	//	    G_CALLBACK (input_cb),
-	//	    oh);
+		               CLUTTER_ACTOR (empty_view->details->cf));
 
 	gtk_widget_show_all (empty_view->details->clutter);
 	/* Only show the actors after parent show otherwise it will just be
 	* unrealized when the clutter foreign window is set. widget_show
 	* will call show on the stage.
 	*/
-	clutter_actor_show_all (CLUTTER_ACTOR (empty_view->details->group));
+	clutter_actor_show_all (CLUTTER_ACTOR (empty_view->details->cf));
 
-	/* Create a timeline to manage animation */
-	timeline = clutter_timeline_new (360, 60); /* num frames, fps */
-	g_object_set(timeline, "loop", TRUE, NULL);   /* have it loop */
-
-	/* fire a callback for frame change */
-	g_signal_connect(timeline, "new-frame",  G_CALLBACK (frame_cb), empty_view->details);
-
-	/* and start it */
-	clutter_timeline_start (timeline);
+	/* FIXME: Test */
+	GFile *testf = g_file_new_for_path("/");
+	clutter_cover_flow_add_gfile(empty_view->details->cf, testf);
 }
 
 static NautilusView *
