e623c62c1cf0b58199663b17efc18bed9aa51539
John Stowers <john.stowers@gmail.com>
2009-05-27 14:42:23 +1200

    Add left and right buttons to ui

--- src/file-manager/main-covflow.c
+++ src/file-manager/main-covflow.c
@@ -7,9 +7,29 @@
 #define YUCK_DEFAULT_WIDTH 1200
 #define YUCK_DEFAULT_HEIGHT 500
 
+gboolean
+on_left_clicked_event (GtkWidget *widget, gpointer user_data)
+{
+    ClutterCoverFlow *cf = CLUTTER_COVER_FLOW(user_data);
+    clutter_cover_flow_left(cf);
+    return FALSE;
+}
+
+gboolean
+on_right_clicked_event (GtkWidget *widget, gpointer user_data)
+{
+    ClutterCoverFlow *cf = CLUTTER_COVER_FLOW(user_data);
+    clutter_cover_flow_right(cf);
+    return FALSE;
+}
+
 int
 main (int argc, char *argv[])
 {
+  GtkWidget *vbox;
+  GtkWidget *bbox;
+  GtkWidget *leftbutton;
+  GtkWidget *rightbutton;
   ClutterActor    *stage;
   ClutterColor     stage_color = { 0x00, 0x00, 0x00, 0xff };
   GtkWidget       *window, *clutter;
@@ -22,20 +42,35 @@ main (int argc, char *argv[])
   g_signal_connect (window, "destroy",
                     G_CALLBACK (gtk_main_quit), NULL);
 
+  vbox = gtk_vbox_new (FALSE, 2);
+  gtk_container_add (GTK_CONTAINER (window), vbox);
+
+  /* Setup stage for gtk+ embedding */
   clutter = gtk_clutter_embed_new ();
   stage = gtk_clutter_embed_get_stage (GTK_CLUTTER_EMBED (clutter));
-
-  gtk_container_add (GTK_CONTAINER (window), clutter);
   gtk_widget_set_size_request (clutter, YUCK_DEFAULT_WIDTH, YUCK_DEFAULT_HEIGHT);
   clutter_actor_set_size(stage, YUCK_DEFAULT_WIDTH, YUCK_DEFAULT_HEIGHT);
-
   /* and its background color */
   clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
 
+  /* Add the important bits */
   cf = clutter_cover_flow_new (stage);
 
-  gtk_widget_show_all (window);
+  /* Buttons and UI */
+  gtk_box_pack_start (GTK_BOX(vbox), clutter, TRUE, TRUE, 0);
+  bbox = gtk_hbox_new (TRUE, 0);
+  gtk_box_pack_start (GTK_BOX(vbox), bbox, FALSE, TRUE, 0);
+  leftbutton = gtk_button_new_from_stock (GTK_STOCK_GO_BACK);
+  g_signal_connect (G_OBJECT (leftbutton), "clicked",
+                    G_CALLBACK (on_left_clicked_event), (gpointer) cf);
+  gtk_box_pack_start (GTK_BOX(bbox), leftbutton, FALSE, TRUE, 0);
+  rightbutton = gtk_button_new_from_stock (GTK_STOCK_GO_FORWARD);
+  g_signal_connect (G_OBJECT (rightbutton), "clicked",
+                    G_CALLBACK (on_right_clicked_event), (gpointer) cf);
+  gtk_box_pack_start (GTK_BOX(bbox), rightbutton, FALSE, TRUE, 0);
 
+  /* Show the important bits */
+  gtk_widget_show_all (window);
   /* Only show the actors after parent show otherwise it will just be
    * unrealized when the clutter foreign window is set. widget_show
    * will call show on the stage.
